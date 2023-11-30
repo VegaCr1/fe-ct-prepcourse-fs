4bea210126f531af8549e73e8d94d438d0a6e8c6
Cristian Vega <105086000+VegaCr1@users.noreply.github.com>
2023-11-29 19:52:31 -0500

    Update Ejercicio 02.js
    corregido por los test ya que daba el resultado pero no lo desarrole con la logique que pedia el test

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -3,6 +3,7 @@
 
 function devolverString(string) {
    // Debe retornar un string.
+   // Tu código:
    return string;
 }
 
@@ -10,29 +11,33 @@ function devolverString(string) {
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
+   // Tu código:
    return x+y;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
-    return x-y;
+   // Tu código:
+   return x-y;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
-     return x/y;
+   // Tu código:
+   return x/y;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
+   // Tu código:
    return x*y;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
+   // Tu código:
    return x%y;
 }
-
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
    devolverString,
